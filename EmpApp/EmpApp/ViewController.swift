74a998ff4d69c68b00ffb00c58cce33fcf5873b7
Rahul Dubey <developerrahuldubey@gmail.com>
2022-07-29 23:04:29 -0500

    Added an empty project
    - Added XCode Project

---
+++ EmpApp/EmpApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmpApp
+//
+//  Created by Rahul Dubey on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
